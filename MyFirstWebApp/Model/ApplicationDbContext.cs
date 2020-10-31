4bcaa03c5cc1a9fed46e6716fe61a1e9b0644a2c
Romario Da Silva <roma.dasilva94@gmail.com>
2020-10-31 17:52:05 -0300

    First migration

---
+++ MyFirstWebApp/Model/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstWebApp.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ MyFirstWebApp/Model/Book.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstWebApp.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
